fe3a4c9c2439f3182ba7cb89a52082fa1e92b372
sparkit2002 <rielyallen@gmail.com>
2018-01-23 18:31:55 -0500

    storage class first commit

--- json_save_system.rb
+++ json_save_system.rb
@@ -3,7 +3,48 @@ require './artist.rb'
 
   class Storage
 
-    def initialize(argument)
-      @argument = argument
+    def initialize()
+      @filename = "./json_storage.json"
+      json = File.read(@filename)
+      @saved = JSON.parse(json)
     end
+
+    def load_artists(json)
+      artists_load = {}
+
+      json.each do |key,value|
+        artists_load[key] = Artist.new(value['name'])
+        tracks = load_tracks(value['tracks'])
+        artists_load[key].tracks = tracks
+      end
+      return artists_load
+    end
+
+    def load_tracks(list)
+      tracks_load = []
+      list.each do |track|
+        tracks_load << Track.new(track['name'],track['artist'])
+      end
+      return tracks_load
+    end
+
+    def store_art(artists)
+      store = {}
+      artists.each do |key,value|
+        store_tracks = store_track(value.tracks)
+        store[key] = {'name' => value.name,'tracks' => store_tracks}
+      end
+
+      return store
+
+    end
+
+    def store_track(tracks)
+      store = []
+      tracks.each do |track|
+        store << {'name' => track.name, 'artist' => track.artist}
+      end
+      return store
+    end
+    
   end
